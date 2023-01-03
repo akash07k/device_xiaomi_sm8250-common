c68816391eafb43d47c86d20519f024340e6c26d
Akash Kakkar <akash.galaxy07@gmail.com>
2023-01-03 19:57:06 +0530

    sm8250-common: Don't apply the patches for MiuiCamera

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,39 +8,3 @@ echo 'Clearing build info'
 # gen_from_buildinfo_sh rule in build/core/sysprop.mk. This will populate
 # system/build.prop file with fresh infos when making "dirty" build.
 rm -vf out/target/product/munch/obj/PACKAGING/system_build_prop_intermediates/buildinfo.prop
-
-# **
-
-patches_dir=$(pwd)/device/xiaomi/sm8250-common/patches
-
-# **
-echo 'Patching frameworks/base'
-patch_dir=frameworks/base
-patch_name='Patch for MIUICamera in framework base'
-cur_commit="$(git -C $patch_dir show -s --format=%s)" || exit $?
-
-# Remove old commit
-if [ "$cur_commit" = "$patch_name" ]; then
-    git -C $patch_dir reset --hard HEAD^ || exit $?
-fi
-
-# Apply and commit patch
-git -C $patch_dir apply --verbose $patches_dir/framework_base/*.patch
-git -C $patch_dir commit --no-gpg-sign -am "$patch_name"
-# **
-
-# **
-echo 'Patching frameworks/av'
-patch_dir=frameworks/av
-patch_name='Patch for MIUICamera in framework av'
-cur_commit="$(git -C $patch_dir show -s --format=%s)" || exit $?
-
-# Remove old commit
-if [ "$cur_commit" = "$patch_name" ]; then
-    git -C $patch_dir reset --hard HEAD^ || exit $?
-fi
-
-# Apply and commit patch
-git -C $patch_dir apply --verbose $patches_dir/framework_av/*.patch
-git -C $patch_dir commit --no-gpg-sign -am "$patch_name"
-# **
